ff209c71c480562c933d5951f58b9680008033dc
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 14:00:13 -0700

    update handleButton event to correctly generate results

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
-// import triviaService from "./trivia.js";
+import triviaService from "./trivia.js";
 
 // UI Logic
 
-// function handleButton(event) {
-//     event.preventDefault();
-//     let promise = triviaService.getTrivia();
-//     promise.then(function (triviaResponse) {
-//         createCards(triviaResponse);
-//     }, function (errorResponse) {
-//         sendError(errorResponse);
-//     });
-// }
+function handleButton(event) {
+    event.preventDefault();
+    document.querySelector("p#error").innerText = "";
+    let promise = triviaService.getTrivia();
+    promise.then(function (triviaResponse) {
+        createCards(triviaResponse);
+    }, function (errorResponse) {
+        sendError(errorResponse);
+    });
+}
 
-// function sendError(response) {
-//     document.querySelector("#error").innerText = `There was an error accessing the trivia data: ${response.status} - ${response.statusText}.`
-// }
+function sendError(error) {
+    document.querySelector("p#error").innerText = `There was an error accessing the trivia data: ${error.status} - ${error.statusText}.`;
+}
 
-// function createCards(response) {
+function createCards(response) {
+    for (let i = 0; i < 5; i++) {
+        document.querySelector(`#question${i}`).innerText = `${response.results[i].question}`;
+    }
+    document.querySelector("#cardResults").removeAttribute("class");
+}
 
-// }
-
-// document.querySelector("#questions").addEventListener("click", handleButton);
+document.querySelector("#questions").addEventListener("click", handleButton);
 
--- src/js/trivia.js
+++ src/js/trivia.js
@@ -2,13 +2,13 @@ export default class triviaService {
     static getTrivia() {
         return new Promise(function (resolve, reject) {
             let request = new XMLHttpRequest();
-            const url = `https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple&token=${process.env.API_TOKEN}`;
+            const url = `https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple`;
             request.addEventListener("loadend", function () {
                 const response = JSON.parse(this.responseText);
                 if (this.status === 200) {
                     resolve(response);
                 } else {
-                    reject(response);
+                    reject(this);
                 }
             });
             request.open("GET", url, true);

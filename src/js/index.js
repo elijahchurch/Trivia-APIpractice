6851d5aa4578fe9c42c685a6c3ccd14546a82ce8
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 11:49:47 -0700

    create trivia Service class with static method

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
-// import //Galaxycalc from './galactic';
+import triviaService from "./trivia.js"
 
--- src/js/trivia.js
+++ src/js/trivia.js
@@ -1,1 +1,18 @@
-// export default class //Galaxycalc {
+export default class triviaService {
+    static getTrivia() {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            const url = `https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple&token=${process.env.API_TOKEN}`;
+            request.addEventListener("loadend", function () {
+                const response = JSON.parse(this.responseText);
+                if (this.status === 200) {
+                    resolve(response);
+                } else {
+                    reject(response);
+                }
+            });
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
+}

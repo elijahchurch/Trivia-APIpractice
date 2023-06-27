9ac552af1ee2c3aeb49886a7d0031215ef4751bb
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 12:05:38 -0700

    start setting up UI logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
-import triviaService from "./trivia.js"
+import triviaService from "./trivia.js";
+
+// UI Logic
+
+function handleButton(event) {
+    event.preventDefault();
+    let promise = triviaService.getTrivia();
+}
+
+// function sendError(response) {
+
+// }
+
+// function createCards(response) {
+
+// }
+
+document.querySelector("#questions").addEventListener("click", handleButton);
 

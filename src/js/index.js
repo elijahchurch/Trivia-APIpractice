9d465b720f27f7da1ea3f3bf1b4999697ccb30ff
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 12:53:44 -0700

    create logic for sendError function

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,16 @@ import triviaService from "./trivia.js";
 function handleButton(event) {
     event.preventDefault();
     let promise = triviaService.getTrivia();
+    promise.then(function (triviaResponse) {
+        createCards(triviaResponse);
+    }, function (errorResponse) {
+        sendError(errorResponse);
+    });
 }
 
-// function sendError(response) {
-
-// }
+function sendError(response) {
+    document.querySelector("#error").innerText = `There was an error accessing the trivia data: ${response.status} - ${response.statusText}.`
+}
 
 // function createCards(response) {
 

66122d4e19268729cd9fb964b2ad9b562067f5ca
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 13:22:22 -0700

    create html elements to contain card info and add styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,27 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
-import triviaService from "./trivia.js";
+// import triviaService from "./trivia.js";
 
 // UI Logic
 
-function handleButton(event) {
-    event.preventDefault();
-    let promise = triviaService.getTrivia();
-    promise.then(function (triviaResponse) {
-        createCards(triviaResponse);
-    }, function (errorResponse) {
-        sendError(errorResponse);
-    });
-}
+// function handleButton(event) {
+//     event.preventDefault();
+//     let promise = triviaService.getTrivia();
+//     promise.then(function (triviaResponse) {
+//         createCards(triviaResponse);
+//     }, function (errorResponse) {
+//         sendError(errorResponse);
+//     });
+// }
 
-function sendError(response) {
-    document.querySelector("#error").innerText = `There was an error accessing the trivia data: ${response.status} - ${response.statusText}.`
-}
+// function sendError(response) {
+//     document.querySelector("#error").innerText = `There was an error accessing the trivia data: ${response.status} - ${response.statusText}.`
+// }
 
 // function createCards(response) {
 
 // }
 
-document.querySelector("#questions").addEventListener("click", handleButton);
+// document.querySelector("#questions").addEventListener("click", handleButton);
 
